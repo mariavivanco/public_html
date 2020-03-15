9eecb1b26ab510c33fef0d78049eff8d39887db6
Sophia <strump@brynmawr.edu>
2020-03-15 17:07:38 -0400

    Delete clean.py

--- clean.py
+++
@@ -1,9 +0,0 @@
-file = open('puzzlesFlatFile.txt').read().split('\n')
-
-outputFile = open('puzzleData.txt', 'w+')
-
-for line in file:
-    line = line.lower()
-    outputFile.write(line + '\n')
-
-outputFile.close()
